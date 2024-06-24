11be7de514e742338f1cad4521d5c2d0204c0905
Eduardo Gualberto <eduardo.d.s.gualberto@gmail.com>
2024-06-24 16:33:16 -0300

    creates minimal repro code for arktype issue

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { type } from "arktype";
 
 export default function App() {
+  const myType = type({
+    test: "string",
+    test2: "number>5",
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
